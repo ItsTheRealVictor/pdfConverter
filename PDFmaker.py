5b7ef498f05eaa7ba172d9179a49a04cf4ff61dc
Victor Delgado <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-12 21:30:36 -0700

    Update PDFmaker.py
    Added an import of 'docx2pdf'. Converts word documents in 1 line. 
    
    Future
    -Will add GUI functionality with Tkinter

--- PDFmaker.py
+++ PDFmaker.py
@@ -1,8 +1,19 @@
 from PIL import Image
-import docx2pdf
+import docx2pdf as dtp
 
-image = Image.open(r'C:\Users\VD102541\Desktop\Barcodes\One.png')
-imageName = 'test'
-imagePDF = image.convert('RGB')
-imagePDF.save(f'C:\\Users\\VD102541\\Desktop\\Barcodes\\{imageName}.pdf')
+HOME = 'c:\\Users\\VD102541\\Desktop\\Misc STuff\\'
+
+dtp.convert(HOME + 'Hello blahblah blah test.docx')
+
+
+
+
+
+
+
+
+# image = Image.open(r'C:\Users\VD102541\Desktop\Barcodes\One.png')
+# imageName = 'test'
+# imagePDF = image.convert('RGB')
+# imagePDF.save(f'C:\\Users\\VD102541\\Desktop\\Barcodes\\{imageName}.pdf')
 print('end')
