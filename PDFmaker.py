67a852f1da37428f94387cdc49c27f1ced28c75d
Victor Delgado <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-21 11:07:12 -0700

    Update PDFmaker.py
    Previous commit does a lot of stuff I don't need. This is a way way way simpler method to do what I want.

--- PDFmaker.py
+++ PDFmaker.py
@@ -1,63 +1,27 @@
-from tkinter import Tk, messagebox as mb, filedialog as fd
-from tkinter.constants import DISABLED, NORMAL
-from tkinter.ttk import Button, Label
+from tkinter import *
+from tkinter import filedialog
 from PIL import Image
-import os
 
 root = Tk()
-root.title("Image to PDF converter")
-root.geometry("500x500")
 
-imglist = []    # For creating a list of images
-fimgl = []  # List for storing multiple image names
-png = True  # If the image chosen is a .png file
+def chooseFile():
+    global choice
+    choice = filedialog.askopenfilenames(title='Choose a file')
+    pickLabel = Label(root, text=choice).pack()
 
-def askfile():
-    global files, fimg, order, imglist, tm, png
-    files = fd.askopenfilenames(title="Choose images", filetypes=(("PNGs", "*.png"), ("JPGs", "*.jpg"), ("All Files", "*.*")))
-    for i in files:
-        fimgl.append(i)
-    if files:
-        for j in fimgl:
-            if j.endswith(".png"):  # If the image is a PNG:
-                png = True
-            fnl = Label(root, text=j)
-            fnl.pack()
-            img = Image.open(j)
-            fimg = img.convert('RGB')
-            imglist.append(fimg)
-            p.config(state=NORMAL)
+pickButton = Button(root, text='Choose File', command=chooseFile).pack()
 
-def convert_pdf():
-    global png
-    try:
-        if png:
-            imglist.pop()
-        saveloc = fd.asksaveasfilename(title="Save the PDF file")
-        if saveloc:
-            if saveloc.endswith(".pdf"):
-                pass
-            else:
-                saveloc = saveloc + ".pdf"
-            if os.path.exists(saveloc):
-                yn = mb.askyesno("Confirm Save As", f"{os.path.basename(saveloc)} already exists.\nDo you want to replace it?")
-                if yn:
-                    os.remove(saveloc)
-                else:
-                    convert_pdf()
-            fimg.save(saveloc, save_all=True, append_images=imglist)
-            mb.showinfo("Done!", "PDF file saved! Click 'OK' to open it")
-            os.startfile(saveloc)
-            root.quit()
-    except Exception as err:
-        mb.showerror("Error", err)
-        root.quit()
+def convertFile():
+    path = choice
+    pdf = Image.open(path)
+    pdf.save(path)
 
-cb = Button(root, text="Add Files", command=askfile)
-cb.pack(pady=20)
 
-p = Button(root, text="Convert", command=convert_pdf, state=DISABLED)
-p.pack(pady=20)
+convertButton = Button(root, text='Convert', command=lambda:convertFile()).pack()
+
+# pdf = Image.open(path)
+# pdf.save(r'C:\Users\VD102541\Desktop\Barcodes\20210601_155926.pdf')
+
 
 
 root.mainloop()
