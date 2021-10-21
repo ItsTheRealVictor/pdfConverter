28d6d0e4a39b5f5fb881be7d37331c0f0960357c
Victor Delgado <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-21 10:22:41 -0700

    Update PDFmaker.py
    Scrapped previous commit's code. 
    
    This is copied from a stackoverflow answer. I don't like the window popups, I'd rather have the files saved in one step after clicking the "save files" button.

--- PDFmaker.py
+++ PDFmaker.py
@@ -1,36 +1,63 @@
-# from PIL import Image
-# import docx2pdf as dtp
+from tkinter import Tk, messagebox as mb, filedialog as fd
+from tkinter.constants import DISABLED, NORMAL
+from tkinter.ttk import Button, Label
+from PIL import Image
+import os
 
-# HOME = 'c:\\Users\\VD102541\\Desktop\\Misc STuff\\'
+root = Tk()
+root.title("Image to PDF converter")
+root.geometry("500x500")
 
-# dtp.convert(HOME + 'Hello blahblah blah test.docx')
+imglist = []    # For creating a list of images
+fimgl = []  # List for storing multiple image names
+png = True  # If the image chosen is a .png file
 
+def askfile():
+    global files, fimg, order, imglist, tm, png
+    files = fd.askopenfilenames(title="Choose images", filetypes=(("PNGs", "*.png"), ("JPGs", "*.jpg"), ("All Files", "*.*")))
+    for i in files:
+        fimgl.append(i)
+    if files:
+        for j in fimgl:
+            if j.endswith(".png"):  # If the image is a PNG:
+                png = True
+            fnl = Label(root, text=j)
+            fnl.pack()
+            img = Image.open(j)
+            fimg = img.convert('RGB')
+            imglist.append(fimg)
+            p.config(state=NORMAL)
 
+def convert_pdf():
+    global png
+    try:
+        if png:
+            imglist.pop()
+        saveloc = fd.asksaveasfilename(title="Save the PDF file")
+        if saveloc:
+            if saveloc.endswith(".pdf"):
+                pass
+            else:
+                saveloc = saveloc + ".pdf"
+            if os.path.exists(saveloc):
+                yn = mb.askyesno("Confirm Save As", f"{os.path.basename(saveloc)} already exists.\nDo you want to replace it?")
+                if yn:
+                    os.remove(saveloc)
+                else:
+                    convert_pdf()
+            fimg.save(saveloc, save_all=True, append_images=imglist)
+            mb.showinfo("Done!", "PDF file saved! Click 'OK' to open it")
+            os.startfile(saveloc)
+            root.quit()
+    except Exception as err:
+        mb.showerror("Error", err)
+        root.quit()
 
+cb = Button(root, text="Add Files", command=askfile)
+cb.pack(pady=20)
 
-# Import Module
-import tkinter as tk
-from tkinter.filedialog import askopenfilenames
-from tkinter.messagebox import showinfo
-from pathlib import Path
-import img2pdf
-  
-# Create Object
-root = tk.Tk()
-root.title('Pick a file, dude')
-root.resizable(True, True)
-root.geometry('400x200')
+p = Button(root, text="Convert", command=convert_pdf, state=DISABLED)
+p.pack(pady=20)
 
-def pickFile():
-    fileNames = askopenfilenames(title='Open Files', initialdir = '/')
-    tk.Label(root, text=f'The File you have chosen is\n{fileNames}').pack()
-
-openButton = tk.Button(root, text = "pick a file", command = pickFile).pack(expand=True)
 
 root.mainloop()
-
-# image = Image.open(r'C:\Users\VD102541\Desktop\Barcodes\One.png')
-# imageName = 'test'
-# imagePDF = image.convert('RGB')
-# imagePDF.save(f'C:\\Users\\VD102541\\Desktop\\Barcodes\\{imageName}.pdf')
-print('end')
