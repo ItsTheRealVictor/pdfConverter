07b0642b61e392eada1bbf2a575861803c9592b8
Victor Delgado <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-22 12:26:26 -0700

    Update PDFmaker.py
    Added with/open statement to handle multiple files.

--- PDFmaker.py
+++ PDFmaker.py
@@ -6,35 +6,21 @@ from PIL import Image
 root = Tk()
 
 
-def singleFile():
-    try:
-        path = fileName[0]
-        pdf = Image.open(path)
-        pdf.save(path + '.pdf')
-    except ValueError:
-        pass
-
-
-def multipleFiles():
-    try:
-        path = fileName
-        for file in fileName:
-            print(str(file))
-            # pdf = Image.open(file)
-            # pdf.save(file + '.pdf')
-    except ValueError:
-        pass
-
-
 def pickFile():
     global fileName
     fileName = askopenfilenames()
 
 def convertFile():
-    if len(fileName) > 1:
-        singleFile()
-    else:
-        multipleFiles()
+    try:
+        path = fileName
+        with open(f'file.pdf', 'wb') as f:
+            for file in fileName:
+                pdf = Image.open(file)
+                pdf.save(file + '.pdf')
+            
+    except ValueError:
+        pass
+    
 
 
 chooseButton = Button(root, text='pick a file', command=pickFile).pack()
