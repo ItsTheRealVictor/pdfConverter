c903b9b1391e39bcf944bbfd360e36f052f2c875
Victor Delgado <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-22 03:35:32 -0700

    Update PDFmaker.py
    Trying to find a solution for multiple file selection, currently only works with single

--- PDFmaker.py
+++ PDFmaker.py
@@ -5,17 +5,36 @@ from PIL import Image
 
 root = Tk()
 
-def pickFile():
-    global fileName
-    fileName = askopenfilenames()
 
-def convertFile():
+def singleFile():
     try:
         path = fileName[0]
         pdf = Image.open(path)
         pdf.save(path + '.pdf')
     except ValueError:
         pass
+
+
+def multipleFiles():
+    try:
+        path = fileName
+        for file in fileName:
+            print(str(file))
+            # pdf = Image.open(file)
+            # pdf.save(file + '.pdf')
+    except ValueError:
+        pass
+
+
+def pickFile():
+    global fileName
+    fileName = askopenfilenames()
+
+def convertFile():
+    if len(fileName) > 1:
+        singleFile()
+    else:
+        multipleFiles()
 
 
 chooseButton = Button(root, text='pick a file', command=pickFile).pack()
